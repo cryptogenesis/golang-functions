34e6ad352bfe3ab52fe0b9957e797b3125100332
EVAN ROSE <evan@cryptogenesis.com>
2016-08-11 18:26:35 -0500

    updated

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strings"
 
 var (
 	author  = "Evan Rose"
@@ -8,5 +9,32 @@ var (
 )
 
 func main() {
-	fmt.Println("Printed")
+	fmt.Println(double(5))
+	first, _ := parseName("Evan Rose")
+	first_non_idiomatic, _ := parseNameNonIdiomatic("Evan Rose")
+	fmt.Println(first)
+	fmt.Println(first_non_idiomatic)
+
+	//anonymous function:
+	greet := func(name string) {
+		fmt.Println("Hello", name, ", how are you today?")
+	}
+
+	greet(first)
+}
+
+func double(n int) int {
+	return n + n
+}
+
+func parseName(name string) (first, last string) {
+	parsed := strings.Split(name, " ")
+	return parsed[0], parsed[1]
+}
+
+//naming the outputs is optional
+
+func parseNameNonIdiomatic(name string) (string, string) {
+	parsed := strings.Split(name, " ")
+	return parsed[0], parsed[1]
 }
